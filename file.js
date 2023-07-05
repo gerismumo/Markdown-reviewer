b7ea54476b154ed3284f657b9713c67eeeb7b8f2
gerald <geraldmumo6@gmail.com>
2023-07-05 19:15:26 +0300

    final commit

--- file.js
+++ file.js
@@ -1,39 +1,33 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const editor = document.getElementById('editor');
-    const preview = document.getElementById('preview');
-  
-    editor.addEventListener('input', () => {
-      const markdown = editor.value;
-      
-      preview.innerHTML = markdown;
-    });
-    const code = `
-    <h1>header1</h1>
-    <h2>header2</h2>
-    <a href="#">home</a>
-    <p>hello <code> alert(hello)</code></p>
-    <ul>
-    <li>hello</li>
-     <li>hello/li>
-   </ul>
-    <pre>
+$(document).ready(() => {
+  const editor = $('#editor');
+  const preview = $('#preview');
 
-     console.log('Hello, world!');
-    </pre>
-    <blockquote>
-       geraldmumo
-    </blockquote>
-    <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Google logo">
-    <p>This <b>Hello, world!</b></p>
+  const updatePreview = () => {
+    const markdown = editor.val();
+    const html = marked(markdown, { breaks: true });
+    preview.html(html);
+  };
 
-    
-    `;
+  editor.on('input', updatePreview);
 
+  const defaultMarkdown = `# header1
+## header2
+[home](#)
+hello \`alert('hello')\`
 
-const currentValue = editor.value = code;
-preview.innerHTML =currentValue;
+- hello
+- hello
 
- 
-  });
+\`\`\`
+console.log('Hello, world!');
+\`\`\`
 
+> geraldmumo
 
+![Google logo](https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png)
+
+This **Hello, world!**`;
+
+  editor.val(defaultMarkdown);
+  updatePreview();
+});
